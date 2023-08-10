c1b7e343b429a59bd89768d7294adcf517556820
p32929 <p32929@yahoo.com>
2023-08-10 18:39:09 +0600

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron')
+// include the Node.js 'path' module at the top of your file
+const path = require('path')
+
+// modify your existing createWindow() function
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
